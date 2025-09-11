d3ae232b21b9a2a13d89d5f8eaeacda66064c3d6
Szabi <nagysz@kkszki.hu>
2025-09-11 13:24:14 +0200

    A menü működése kész

--- MenuConsole/Program.cs
+++ MenuConsole/Program.cs
@@ -12,7 +12,7 @@ namespace MenuConsole
         {
             string nev = "";
             bool ffi = true;
-            int szulev;
+            int szulev = 0;
 
             string valasz = "";
             do
@@ -28,20 +28,50 @@ namespace MenuConsole
 
                 if (valasz == "1")
                 {
+                    Console.WriteLine("Név: ");
+                    nev = Console.ReadLine();
+
+                    Console.WriteLine("Neve (f/n): ");
+                    ffi = Console.ReadLine() == "f";
+
+                    string szulevStr = "";
+                    do
+                    {
+                        Console.WriteLine("Születési év: ");
+                        szulevStr = Console.ReadLine();
+                    } while (!int.TryParse(szulevStr, out szulev));
+                    szulev = int.Parse(szulevStr);
+
                     Console.WriteLine("Sikeres adatrögzírtés! Folytatáshoz nyomjon Entert. ");
                     Console.ReadLine();
 
+
                 }else if (valasz == "2")
                 {
+                    string nem = "";
+                    if (ffi == true)
+                    {
+                        nem = "Férfi";
+                    }else
+                    {
+                        nem = "Nő";
+                    }
+                    Console.WriteLine($"Név: {nev} \n" +
+                        $"Neme: {nem}\n" +
+                        $"Születési év: {szulev}");
+                    Console.ReadLine();
 
 
                 }else if (valasz == "3")
                 {
+                    Console.WriteLine("Biztosan ki szeretne lépni? (i/n)");
+                    valasz = Console.ReadLine() != "i" ? " " : "3";
 
 
                 }else
                 {
                     Console.WriteLine("Nem létező menüpont!");
+                    Console.ReadLine();
                 }
 
             } while (valasz != "3");
