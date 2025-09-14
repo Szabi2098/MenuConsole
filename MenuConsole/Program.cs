15949f864a21ed3cca7cdf2722cffa0b16cb71e8
Szabolcs Nagy <Szabi2098@gmail.com>
2025-09-14 11:13:27 +0200

    Áthelyezések

--- MenuConsole/Program.cs
+++ MenuConsole/Program.cs
@@ -10,8 +10,8 @@ namespace MenuConsole
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.DarkMagenta; // sötét lila háttér
-            Console.ForegroundColor = ConsoleColor.White;       // fehér betűszín
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Clear();
             string nev = "";
             bool ffi = true;
@@ -32,49 +32,66 @@ namespace MenuConsole
                 Console.WriteLine("3 - Kilépés");
                 Console.SetCursorPosition(8, 6);
                 Console.WriteLine("Menüpont számával választhat");
+                Console.SetCursorPosition(8, 7);
                 valasz = Console.ReadLine();
 
                 if (valasz == "1")
                 {
                     Console.Clear();
+                    Console.SetCursorPosition(8, 0);
                     Console.WriteLine("Név: ");
+                    Console.SetCursorPosition(8, 1);
                     nev = Console.ReadLine();
 
+                    Console.SetCursorPosition(8, 2);
                     Console.WriteLine("Neme (f/n): ");
+                    Console.SetCursorPosition(8, 3);
                     ffi = Console.ReadLine() == "f";
 
                     string szulevStr = "";
                     do
                     {
+                        Console.SetCursorPosition(8, 4);
                         Console.WriteLine("Születési év: ");
+                        Console.SetCursorPosition(8, 5);
                         szulevStr = Console.ReadLine();
                     } while (!int.TryParse(szulevStr, out szulev));
                     szulev = int.Parse(szulevStr);
 
+                    Console.SetCursorPosition(8, 7);
                     Console.WriteLine("Sikeres adatrögzírtés! Folytatáshoz nyomjon Entert. ");
+                    Console.SetCursorPosition(8, 8);
                     Console.ReadLine();
 
 
                 }else if (valasz == "2")
                 {
                     Console.Clear();
-                    Console.WriteLine($"Név: {nev} \n" +
-                        $"Neme: {(ffi ? "Férfi" : "Nő")}\n" +
-                        $"Születési év: {szulev}");
+                    Console.SetCursorPosition(8, 0);
+                    Console.WriteLine($"Név: {nev}");
+                    Console.SetCursorPosition(8, 1);
+                    Console.WriteLine($"Neme: {(ffi ? "Férfi" : "Nő")}");
+                    Console.SetCursorPosition(8, 2);
+                    Console.WriteLine($"Születési év: {szulev}");
+                    Console.SetCursorPosition(8, 4);
                     Console.ReadLine();
 
 
                 }else if (valasz == "3")
                 {
                     Console.Clear();
+                    Console.SetCursorPosition(8, 0);
                     Console.WriteLine("Biztosan ki szeretne lépni? (i/n)");
+                    Console.SetCursorPosition(8, 1);
                     valasz = Console.ReadLine() != "i" ? " " : "3";
 
 
                 }else
                 {
                     Console.Clear();
+                    Console.SetCursorPosition(8, 0);
                     Console.WriteLine("Nem létező menüpont!");
+                    Console.SetCursorPosition(8, 1);
                     Console.ReadLine();
                 }
 
